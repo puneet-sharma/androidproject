796789689f82583e983d0aaabfcabc116e0f6f19
puneetsharma <arien.puneet@gmail.com>
2013-04-14 04:12:39 -0400

    blah

--- FriendFinder/src/edu/umbc/cmsc628/friendfinder/MapActivity.java
+++ FriendFinder/src/edu/umbc/cmsc628/friendfinder/MapActivity.java
@@ -1,11 +1,27 @@
 package edu.umbc.cmsc628.friendfinder;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.ResponseHandler;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.BasicResponseHandler;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.support.v4.app.NavUtils;
@@ -114,6 +130,43 @@ public class MapActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	private static class sessionPostTask extends AsyncTask<String, Long, String>{
+
+		@Override
+		protected String doInBackground(String... arg0) {
+			String[] credentials = arg0;
+			HttpClient client = new DefaultHttpClient();
+			HttpPost post = new HttpPost("http://130.85.251.231/db.php");
+			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+			pairs.add((NameValuePair) new BasicNameValuePair("latitude", credentials[0]));
+			pairs.add((NameValuePair) new BasicNameValuePair("longitude", credentials[1]));
+			pairs.add((NameValuePair) new BasicNameValuePair("time", credentials[2]));
+			pairs.add((NameValuePair) new BasicNameValuePair("id", "puneetsharma"));
+
+			try {
+				post.setEntity(new UrlEncodedFormEntity(pairs));
+			} catch (UnsupportedEncodingException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			ResponseHandler<String> responseHandler=new BasicResponseHandler();
+			String responseBody = "default";
+			try {
+				responseBody = client.execute(post, responseHandler);
+			} catch (ClientProtocolException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//Log.d("ResponseBody", responseBody);
+			return responseBody;
+		}	
+          
 	}
 
 }
